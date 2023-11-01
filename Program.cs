35c16ddfd62d5ec697ae303b346a5e25e01f0867
TuongTLC <53476330+TuongTLC@users.noreply.github.com>
2023-11-01 10:27:48 +0700

    Init

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using LeetCode;
+
+Easy easyProblems = new Easy();
+
+int[] nums = {3,3};
+int[] output = easyProblems.TwoSum(nums, 6);
+Console.WriteLine(output[0]+","+output[1]);
---
+++ TwoSum.cs
@@ -0,0 +1,19 @@
+namespace LeetCode;
+
+public class Easy
+{
+    //Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    public int[] TwoSum(int[] nums, int target)
+    {
+        for(int i = 0; i < nums.Length-1; i++){
+            for(int j = i+1; j < nums.Length; j++){
+                if( nums[i]+nums[j] == target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        throw new Exception("Not found!!!");
+    }
+}
+        
+    
