c9c47058a6d42916310c5a676e4b7a337ba528eb
TuongTLC <53476330+TuongTLC@users.noreply.github.com>
2023-11-01 12:54:24 +0700

    +2 easy

--- EasyProblems.cs
+++ EasyProblems.cs
@@ -1,8 +1,12 @@
+using System.Numerics;
+
 namespace LeetCode;
 
 public class EasyProblems
 {
-    //Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    //TwoSum
+    //Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to
+    //target.
     public int[] TwoSum(int[] nums, int target)
     {
         for(int i = 0; i < nums.Length-1; i++){
@@ -14,6 +18,37 @@ public class EasyProblems
         }
         throw new Exception("Not found!!!");
     }
+    
+    
+    //Length of Last Word
+    //Given a string s consisting of words and spaces, return the length of the last word in the string.
+    public int LengthOfLastWord(string s)
+    {
+        string lastString = s.Trim().Split(" ").Last();
+        return lastString.Length;
+    }
+    //Plus One
+    //You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of
+    //the integer. The digits are ordered from most significant to least significant in left-to-right order. The large
+    //integer does not contain any leading 0's.
+    public int[] PlusOne(int[] digits)
+    {
+        int n = digits.Length;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (digits[i] < 9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] newNumber = new int[n + 1];
+        newNumber[0] = 1;
+
+        return newNumber;
+    }
+    
 }
         
     
--- Program.cs
+++ Program.cs
@@ -2,6 +2,21 @@
 
 EasyProblems easyProblems = new EasyProblems();
 
-int[] nums = {3,3};
-int[] output = easyProblems.TwoSum(nums, 6);
-Console.WriteLine(output[0]+","+output[1]);
+//***TwoSum***
+// int[] nums = {3,3};
+// int[] output = easyProblems.TwoSum(nums, 6);
+// Console.WriteLine(output[0]+","+output[1]);
+
+
+//***Length of Last Word***
+// string s = "   fly me   to   the moon  ";
+// Console.WriteLine(easyProblems.LengthOfLastWord(s));
+
+//***Plus One***
+// int[] digits = {7,9};
+// int[] output = easyProblems.PlusOne(digits);
+// foreach (var num in output)
+// {
+//     Console.Write(num);
+// }
+
